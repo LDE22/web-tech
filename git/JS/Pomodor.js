b5b7078c8680eca6c9f754550c603856966802f5
Лобанов Даниил Евгеньевич <LDE.22@free.uni-dubna.ru>
2023-11-28 16:09:25 +0300

    Test

--- git/JS/Pomodor.js
+++ git/JS/Pomodor.js
@@ -9,5 +9,29 @@ let buttons = document.querySelectorAll('.btn');
 let set;
 let active='focus';
 let minCount = 24;
-time.TextContent = `${minCount+1}:00`;
+let count = 59;
+let paused = true;
 const appendZero = (value)=>{value=value<10?`0${value}`:value; return value;};
+btnstart.addEventListener('click',()=>{
+    btnstart.classList.remove('show');
+    btnstart.classList.add('hidden');
+    btnpause.classList.add('show');
+    btnreset.classList.add('show');
+    if(paused){
+        paused=false;
+        time.textContent=`${appendZero(minCount)}:${appendZero(count)}`
+        set=setInterval(()=>{
+            if(minCount==0 && count==0){clearInterval(set)}
+            else{if(count!=0){count--;time.textContent=`${appendZero(minCount)}:${appendZero(count)}`;}
+                else{count=60; minCount--;time.textContent=`${appendZero(minCount)}:${appendZero(count)}`;}}
+        },1000)
+    }
+});
+btnpause.addEventListener('click',()=>{
+    btnpause.classList.remove('show');
+    btnpause.classList.add('hidden');
+    btnstart.classList.add('show');
+    btnreset.classList.add('show');
+    paused=true;
+    set=setInterval(()=>{time.textContent=`${appendZero(minCount)}:${appendZero(count)}`;},0);
+});
